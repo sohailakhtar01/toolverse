b2a5a06ee13f3ad43c3020643bccfaefbd10fe45
sohailakhtar01 <sohailakhtar1326@gmail.com>
2025-12-24 23:53:11 +0530

    added few contents

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,75 +1,55 @@
 "use client";
 
-import { useEffect } from 'react';
-import { usePathname, useSearchParams } from 'next/navigation';
-import NProgress from 'nprogress';
+import { useEffect } from "react";
+import { usePathname } from "next/navigation";
+import NProgress from "nprogress";
 
-// Custom CSS inline - no separate file needed!
-if (typeof window !== 'undefined') {
-  const style = document.createElement('style');
+if (typeof window !== "undefined") {
+  const style = document.createElement("style");
   style.innerHTML = `
-    #nprogress {
-      pointer-events: none;
-    }
+    #nprogress { pointer-events: none; }
     #nprogress .bar {
       background: linear-gradient(to right, #9333ea, #ec4899);
       position: fixed;
       z-index: 9999;
-      top: 0;
-      left: 0;
+      top: 0; left: 0;
       width: 100%;
       height: 3px;
-    }
-    #nprogress .peg {
-      display: block;
-      position: absolute;
-      right: 0px;
-      width: 100px;
-      height: 100%;
-      box-shadow: 0 0 10px #ec4899, 0 0 5px #ec4899;
-      opacity: 1;
-      transform: rotate(3deg) translate(0px, -4px);
     }
   `;
   document.head.appendChild(style);
 }
 
-// Configuration
-NProgress.configure({ 
+NProgress.configure({
   showSpinner: false,
   speed: 400,
-  minimum: 0.3
+  minimum: 0.3,
 });
 
 export default function ProgressBar() {
   const pathname = usePathname();
-  const searchParams = useSearchParams();
 
   useEffect(() => {
     NProgress.done();
-  }, [pathname, searchParams]);
+  }, [pathname]);
 
   useEffect(() => {
-    const handleAnchorClick = (e) => {
-      const targetUrl = e.currentTarget.href;
-      const currentUrl = window.location.href;
-      if (targetUrl !== currentUrl) {
+    const handleClick = (e) => {
+      const target = e.currentTarget.href;
+      if (target !== window.location.href) {
         NProgress.start();
       }
     };
 
-    const handleMutation = () => {
-      const anchorElements = document.querySelectorAll('a[href]');
-      anchorElements.forEach(anchor => {
-        anchor.addEventListener('click', handleAnchorClick);
-      });
+    document.querySelectorAll("a[href]").forEach((a) =>
+      a.addEventListener("click", handleClick)
+    );
+
+    return () => {
+      document.querySelectorAll("a[href]").forEach((a) =>
+        a.removeEventListener("click", handleClick)
+      );
     };
-
-    const mutationObserver = new MutationObserver(handleMutation);
-    mutationObserver.observe(document, { childList: true, subtree: true });
-    handleMutation();
-
-    return () => mutationObserver.disconnect();
   }, []);
 
   return null;
